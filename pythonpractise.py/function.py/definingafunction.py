979bee285fdd3908ec92c628de05c8d3efb2c769
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-03 23:32:02 +0530

    changes

---
+++ pythonpractise.py/function.py/definingafunction.py
@@ -0,0 +1,44 @@
+# defining a function
+
+def func():
+    result = "the strinng tpo be used"
+    print(result)
+
+# func();
+
+# calling a function:
+# defining a function gives it a name
+
+def func():
+    result = "this is me"
+    print(result)
+
+func()
+
+# // pass by value vs value
+def change(list):
+    print("print my list:", list)
+    list[2] = 500;
+    print(list)
+
+list = [10,20,30]
+change(list)
+
+print(list)
+
+
+
+def a(b):
+    print(b)
+a(5);
+
+print(list)
+
+# required arguments :
+
+def printMe(str):
+    print(str)
+
+printMe()
+
+# 
