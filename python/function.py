295b872006e489ddf11b09447d7e8ae898e133f0
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-03 23:38:08 +0530

    changes

---
+++ python/function.py
@@ -0,0 +1,16 @@
+def getInteger():
+    result = "hello wworld"
+    return result 
+
+def main():
+    print("output standard")
+    output = getInteger()
+    print(output)
+
+if __name__=="__main__":
+    main()
+
+    def step():
+        for x in range(5):
+            print(x)
+    step()
