aa95cbb2c20a8b1b22203529c3c1000379e269a0
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-09 21:32:54 +0530

    changes

---
+++ python/pythonenginercoreyadvanced/tuple.py
@@ -0,0 +1,43 @@
+tuple = (1,2,3)
+#accesing the values of tuples
+print(tuple)
+# tuples[0] = 100 
+# # in python tuples are immutable means they cannot be modified .
+# print(tuples)
+
+# delete tuples elements:
+
+del tuple
+print(tuple)
+
+tuple1 = ("max",46,"mike",50);
+# item = tuple1[::-1]
+print(tuple1[::-1])
+
+tuple2 = (10,21,21,23);
+
+for i in tuple2:
+    print(i,end=" ");
+
+
+if 10 in tuple2:
+    print("yes there");
+
+tup = ("a","b","c","d","c");
+print(tup.count("d"));
+
+print(tup.index("c"))
+
+tupp = [2,1,3]
+mytup = list(tupp);
+print(mytup)
+
+print(tuple(mytup));
+
+
+mytup3 = (1,2,3)
+print(mytup3[::-1])
+
+
+
+
