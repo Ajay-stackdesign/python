e11db882ccb6b829db41700176d338cc875d2732
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-11 23:42:37 +0530

    changes

---
+++ python/dsapython/dictoanries.py
@@ -0,0 +1,26 @@
+dict = {
+    "name": "ajay",
+    "age": 15,
+    "house": "balaram",
+    "working": "fulstack"
+}
+
+# accesing key and values of dictonaries
+print(dict)
+
+# dict["name"]
+print(dict["name"])
+
+print(dict["working"])
+
+# Updating the dictionary:
+
+dict["name"] = "geeta";
+print(dict["name"]);
+
+# deleting  a dictionary:
+
+del dict["name"]
+
+print(dict)
+
