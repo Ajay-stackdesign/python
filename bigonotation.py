00ab55f973baacbf9152989294f4485e0972d764
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-22 23:10:09 +0530

    chages

---
+++ bigonotation.py
@@ -0,0 +1,13 @@
+
+
+def duplicatesame(number):
+    duplicate = 1
+    for  i in range(len(number)):
+        if number[i] == duplicate:
+            print(i);
+
+duplicatesame([2,3,4,5,7,1]);
+
+
+
+
