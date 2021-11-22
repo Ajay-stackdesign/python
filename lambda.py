ffcb1ca97167a6b9252f91cd71bf5cbffa7ec633
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-22 23:12:51 +0530

    changes

---
+++ lambda.py
@@ -0,0 +1,33 @@
+sum = lambda a, b : a+b
+
+function =sum(10,12)
+print(function)
+
+
+
+# 12
+# insert 0 5
+# insert 1 10
+# insert 0 6
+# print
+# remove 6
+# append 9
+# append 1
+# sort
+# print
+# pop
+# reverse
+# print
+# Sample Output 0
+
+# [6, 5, 10]
+# [1, 5, 9, 10]
+# [9, 5, 1]
+
+
+def list_problem():
+    list = [];
+    list.insert(0,5)
+    print(list)
+
+list_problem()
