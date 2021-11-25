7e99c49440e7b8757644d50c7b9bd1354860d994
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-25 23:16:53 +0530

    changes

---
+++ python/pythonenginercoreyadvanced/set.py
@@ -0,0 +1,30 @@
+# set = {1,2,3,4}
+# print(set)
+
+# myset = set()
+# myset.add(1)
+# myset.add(2)
+# myset.add(3)
+# myset.remove(1)
+
+# print(myset)
+# myset.clear()
+# print(myset)
+
+# myset = set()
+# myset.add(1)
+# myset.add(2)
+# myset.add(3)
+# print(myset)
+
+# myset.pop()
+# print(myset)
+
+# myset1 = {"hello","string","world"}
+myset= set()
+myset.add("hello")
+myset.add("happy")
+myset.add("world")
+myset.pop()
+print(myset)
+
