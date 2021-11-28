1d9ea78e9066039e86f6e5d9956b82c7651078e9
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-28 23:05:22 +0530

    changes

---
+++ basicoperator/multiplevarible.py
@@ -0,0 +1,11 @@
+x,y = "ajay","sahani"
+
+print(x)
+print(y)
+
+
+price  = 12
+model = "quantity bassic"
+working = "in usa"
+name = "i ama {1} with price {0} andd workiing {2}";
+print(name.format(price,model,working))
