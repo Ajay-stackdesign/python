838ca4b7821d0c1ee255aa9e212bc77aa4a486a6
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-01-14 23:30:06 +0530

    changes

---
+++ stack/stack.js
@@ -0,0 +1,30 @@
+
+
+// stack!!
+// // function : push,pop,peek,length
+// let car = []
+
+// let word = "racecar"
+
+// let rword = ""
+
+// // for push first in first out 
+// for (let i = 0; i < word.length; i++) {
+//     car.push(word[i])
+// }
+
+// //removing the letter from the stack
+// for (let i = 0; i < word.length; i++) {
+//     rword += car.pop()
+// }
+
+// if (rword === word) {
+//     console.log(`${word} is a palindrome`)
+// } else {
+//     console.log(`${word} is not a palindrome`)
+// }
+element = []
+element.append(4)
+element.append(5)
+print(element)
+
